50ce8393d2a2f9de8a658620392d1ba7863261ab
WmIsuru2001 <155750111+WmIsuru2001@users.noreply.github.com>
2025-05-16 23:38:13 +0530

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:workout_planner/pages/calculator.dart';
+import 'package:workout_planner/pages/add_new_page.dart';
+import 'package:workout_planner/pages/favourites_page.dart';
+import 'package:workout_planner/pages/home_page.dart';
+import 'package:workout_planner/pages/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const AddNewPage(),
+    const FavouritesPage(),
+    const ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      title: "Workout Planner",
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: "Add",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Favorites",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
+          ],
+        ),
+        body: _pages[_currentIndex],
       ),
-      home: const Calculator(),
     );
   }
 }
