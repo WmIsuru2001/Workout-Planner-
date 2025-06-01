f084268263d37d7b44ed97c21935a3a7e824e3b3
WmIsuru2001 <155750111+WmIsuru2001@users.noreply.github.com>
2025-06-01 22:42:09 +0530

    add data

---
+++ lib/data/equipment_data.dart
@@ -0,0 +1,56 @@
+import 'package:workout_planner/models/equipment_models.dart';
+
+class EquipmentData {
+  final List<Equipments> equipmentList = [
+    Equipments(
+      id: 0,
+      equipmentName: "Dumbbells",
+      equipmentDescription:
+          "A pair of dumbbells for strength training exercises.",
+      equipmentImageUrl: "assets/images/equipments/bench-press_7922198.png",
+      noOfMinuites: 5,
+      noOfCalaries: 2,
+      handOvered: false,
+    ),
+    Equipments(
+      id: 1,
+      equipmentName: "Dumbbells",
+      equipmentDescription:
+          "A pair of dumbbells for strength training exercises.",
+      equipmentImageUrl: "assets/images/equipments/bench-press_7922198.png",
+      noOfMinuites: 5,
+      noOfCalaries: 2,
+      handOvered: false,
+    ),
+    Equipments(
+      id: 2,
+      equipmentName: "Dumbbells",
+      equipmentDescription:
+          "A pair of dumbbells for strength training exercises.",
+      equipmentImageUrl: "assets/images/equipments/bench-press_7922198.png",
+      noOfMinuites: 5,
+      noOfCalaries: 2,
+      handOvered: false,
+    ),
+    Equipments(
+      id: 3,
+      equipmentName: "Dumbbells",
+      equipmentDescription:
+          "A pair of dumbbells for strength training exercises.",
+      equipmentImageUrl: "assets/images/equipments/bench-press_7922198.png",
+      noOfMinuites: 5,
+      noOfCalaries: 2,
+      handOvered: false,
+    ),
+    Equipments(
+      id: 4,
+      equipmentName: "Dumbbells",
+      equipmentDescription:
+          "A pair of dumbbells for strength training exercises.",
+      equipmentImageUrl: "assets/images/equipments/bench-press_7922198.png",
+      noOfMinuites: 5,
+      noOfCalaries: 2,
+      handOvered: false,
+    ),
+  ];
+}
---
+++ lib/data/exercise_data.dart
@@ -0,0 +1,41 @@
+import 'package:workout_planner/models/exercise_models.dart';
+
+class ExerciesData {
+  final List<Exercise> exerciseList = [
+    Exercise(
+      id: 0,
+      exerciseName: "Push-Ups",
+      exerciseImageUrl: "assets/images/exercises/treadmill_2382679.png",
+      noOfMinuites: 10,
+      completed: false,
+    ),
+    Exercise(
+      id: 1,
+      exerciseName: "Push-Ups",
+      exerciseImageUrl: "assets/images/exercises/treadmill_2382679.png",
+      noOfMinuites: 10,
+      completed: false,
+    ),
+    Exercise(
+      id: 2,
+      exerciseName: "Push-Ups",
+      exerciseImageUrl: "assets/images/exercises/treadmill_2382679.png",
+      noOfMinuites: 5,
+      completed: false,
+    ),
+    Exercise(
+      id: 3,
+      exerciseName: "Push-Ups",
+      exerciseImageUrl: "assets/images/exercises/treadmill_2382679.png",
+      noOfMinuites: 15,
+      completed: false,
+    ),
+    Exercise(
+      id: 4,
+      exerciseName: "Push-Ups",
+      exerciseImageUrl: "assets/images/exercises/treadmill_2382679.png",
+      noOfMinuites: 10,
+      completed: false,
+    ),
+  ];
+}
